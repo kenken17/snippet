ade49c78b44964909040eab1b960b1dc2ed29896
ken <kenneth.lee@comwerks.com>
2012-06-08 10:23:42 +0800

    - add get query param

--- functions.js
+++ functions.js
@@ -12,3 +12,43 @@ function IsDigit(digit) {
     var filter = /^\s*(\+|-)?\d+\s*$/;
     return filter.test(digit);
 }       
+
+function getMonth(digit) {
+    var month = new Array();
+        month[0] = "Jan";
+        month[1] = "Feb";
+        month[2] = "Mar";
+        month[3] = "Apr";
+        month[4] = "May";
+        month[5] = "Jun";
+        month[6] = "Jul";
+        month[7] = "Aug";
+        month[8] = "Sep";
+        month[9] = "Oct";
+        month[10] = "Nov";
+        month[11] = "Dec";            
+        
+    return month[digit];
+}
+
+/* Usage: decodeEntities($('<span></span>').html($('id').text())) */
+function decodeEntities(input) {
+    var y = document.createElement('textarea');
+    y.innerHTML = input;
+    
+    return y.value;
+}
+
+// Get query param
+function getParameterByName(name) { 
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    
+    if(results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+};
